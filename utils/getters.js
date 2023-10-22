644cb769c202b20db2475b8756882622fc6ee316
milo2112 <mjrp.ros@gmail.com>
2023-10-22 11:23:03 -0300

    provide HATEOAS structure for client navegation

--- utils/getters.js
+++ utils/getters.js
@@ -1,1 +1,17 @@
 const { executeQuery } = require('./pg')
+
+const { HATEOAS } = require('./provideHATEOAS')
+
+const getJewels = async () => {
+  const queryResult = await executeQuery('SELECT * FROM inventario;')
+  return HATEOAS(queryResult)
+}
+
+const getJewelById = async (id) => {
+  return await executeQuery('SELECT * FROM inventario WHERE id = $1;', [id])
+}
+
+module.exports = {
+  getJewels,
+  getJewelById
+}
