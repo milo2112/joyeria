97a901585b99c92f9912ed00b125c4ca5b07f1f6
milo2112 <mjrp.ros@gmail.com>
2023-10-22 22:44:02 -0300

    refactoring HATEOAS method

--- utils/getters.js
+++ utils/getters.js
@@ -4,13 +4,14 @@ const format = require('pg-format')
 const { HATEOAS } = require('./provideHATEOAS')
 
 const getJewels = async ({ limit = 6, page = 1, order = 'id_ASC' }) => {
-  const totalRecords = await getTotalRecords()
   const queryString = { limit, page, order }
+  const totalRecords = await getTotalRecords()
+  queryString.count = Number(totalRecords[0].count)
   const [field, direction] = order.split('_')
   const offSet = (limit * (page - 1))
   const query = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s;', field, direction, limit, offSet)
   const queryResult = await executeQuery(query)
-  return HATEOAS(queryResult, queryString, totalRecords)
+  return HATEOAS(queryResult, queryString)
 }
 
 const getTotalRecords = async () => await executeQuery('SELECT count(id) FROM inventario;')
--- utils/provideHATEOAS.js
+++ utils/provideHATEOAS.js
@@ -1,4 +1,4 @@
-const HATEOAS = async (jewels, { limit, page, order }, totalRecords) => {
+const HATEOAS = async (jewels, { limit, page, order, count }) => {
   const inventory = jewels.map((item) => ({
     name: item.nombre,
     href: `/joyas/joya/${item.id}`
@@ -6,7 +6,7 @@ const HATEOAS = async (jewels, { limit, page, order }, totalRecords) => {
   const qryStrText = `/joyas?limit=${limit}&order=${order}&page=`
   return {
     total: inventory.length,
-    next: ((page * limit) < totalRecords[0].count) ? `${qryStrText}${+page + 1}` : null,
+    next: ((page * limit) < count) ? `${qryStrText}${+page + 1}` : null,
     previous: page > 1 ? `${qryStrText}${page - 1}` : null,
     inventory
   }
